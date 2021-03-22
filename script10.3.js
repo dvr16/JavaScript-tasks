61a3df0b28a72480f4d7c7b3bbe3b3b1579cffdf
Дмитрий <dvr07@yandex.ru>
2021-03-22 14:43:51 +0300

    Ф-ия, которая отнимает от 1 числа 2 и / на третье.

---
+++ script10.3.js
@@ -0,0 +1,11 @@
+/*
+Сделайте функцию, которая возвращает квадрат числа. 
+Число передается параметром.
+*/
+
+function res(numb1, numb2, numb3) {
+    let result = (numb1 - numb2) / numb3;
+    alert(result);
+}
+
+res(10, 4, 3);
