948797b2bdd656342e84d90c7f5a121be19b204f
Дмитрий <dvr07@yandex.ru>
2021-04-08 12:51:22 +0300

    new

---
+++ JavaScript2.1.js
@@ -0,0 +1,22 @@
+/*
+Number
+String
+Boolean
+Object
+Null
+Undefined
+*/
+
+var num = 42;
+var string = 'Message';
+var isTrue = false;
+var obj = { a: 1 };
+var isNull = null;
+var undef = undefined;
+
+console.log(typeof num);
+console.log(typeof string);
+console.log(typeof isTrue);
+console.log(typeof obj);
+console.log(typeof isNull);
+console.log(typeof undef);
